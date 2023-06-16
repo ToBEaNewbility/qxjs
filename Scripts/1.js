aff1c64722ee9865806cc7a4636f7d2482aaf2f0
ToBEaNewbility <104293833+ToBEaNewbility@users.noreply.github.com>
2023-06-16 14:34:24 +0800

    Update 1.js

--- Scripts/1.js
+++ Scripts/1.js
@@ -1,7 +1,7 @@
 var body = $response.body; // 声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
 
-obj.res.vip=true;
+obj.res.isvip=true;
 obj.res.viptime=1987654321;
 
 body = JSON.stringify(obj); // 重新打包回json字符串
